131cd73a3f36ff20386f08d3dd8bf1b8cd5a414b
조민형 <whalsguddlek@gmail.com>
2026-01-25 03:16:50 +0900

    2026-01-25 complete

--- baeckjoon/로또/solution.py
+++ baeckjoon/로또/solution.py
@@ -1,27 +1,26 @@
-import sys
-input = sys.stdin.readline
 
 
 def dfs(start) :
     if len(path) == 6 :
         print(*path)
         return
-    
+        
     for i in range(start, k) :
-        path.append(nums[i])
-        dfs(i + 1) 
+        path.append(arr[i])
+        dfs(i + 1)
         path.pop()
-
+    
 while True :
-    lotto = input().split()
-    if not lotto :
+    n = list(map(int, input().split()))
+    if not n :
         break
     
-    k = int(lotto[0])
+    k = n[0]
     if k == 0 :
         break
     
-    nums = list(map(int, lotto[1:]))
+    arr = n[1:]
     path = []
+    
     dfs(0)
     print()
