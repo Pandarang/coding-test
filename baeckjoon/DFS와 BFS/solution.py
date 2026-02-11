acb3959b28e5379171e2a99d57756585136f73f7
조민형 <whalsguddlek@gmail.com>
2026-02-11 18:06:34 +0900

    타겟 넘버

--- baeckjoon/DFS와 BFS/solution.py
+++ baeckjoon/DFS와 BFS/solution.py
@@ -1,48 +1,41 @@
 from collections import deque
 
-a, b, c = map(int, input().split())
+n, m, v = map(int, input().split())
 
-graph1 = [[] for _ in range(a + 1)]
-visited1 = [False] * (a + 1)
-graph2 = [[] for _ in range(a + 1)]
-visited2 = [False] * (a + 1)
-arr1, arr2 = [], []
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n + 1)
 
-for _ in range(b) :
+for _ in range(m) :
     x, y = map(int, input().split())
-    graph1[x].append(y)
-    graph1[y].append(x)
-    graph2[x].append(y)
-    graph2[y].append(x)
+    graph[x].append(y)
+    graph[y].append(x)
 
-for i in range(1, a+1) :
-    graph1[i].sort()
-    graph2[i].sort()
+for i in range(1, n+1) :
+    graph[i].sort() 
 
 def dfs(x) :
-    visited1[x] = True
-    print(x, end=' ')
+    visited[x] = True
+    print(x, end=" ")
+    
+    for nx in graph[x] : 
+        if not visited[nx] :
+            dfs(nx)
 
-    for nxt in graph1[x] :
-        if not visited1[nxt] :
-            dfs(nxt)
 
 def bfs(start) :
-    visited2[start] = True
     q = deque([start])
+    visited = [False] * (n + 1)
+    visited[start] = True
 
     while q :
         x = q.popleft()
+        print(x, end=' ')
+        
+        for nx in graph[x] :
+            if not visited[nx] :
+                visited[nx] = True
+                q.append(nx)
 
-        for nxt in graph2[x] :
-            if not visited2[nxt] :
-                visited2[nxt] = True
-                q.append(nxt)
-                arr2.append(nxt)
-
-dfs(c)
-print(*arr1)
-
-arr2.append(c)
-bfs(c)
-print(*arr2)
+dfs(v)
+print()
+bfs(v)
