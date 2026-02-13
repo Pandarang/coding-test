684634137cd4612a6991cbb0472ee6d244b25f33
Pandarang <whalsguddlek@gmail.com>
2026-02-13 23:51:57 +0900

    review 바이러스

--- baeckjoon/바이러스/solution.py
+++ baeckjoon/바이러스/solution.py
@@ -4,20 +4,20 @@ m = int(input())
 graph = [[] for _ in range(n + 1)]
 visited = [False] * (n + 1)
 
+for i in range(1, m + 1) :
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for _ in range(m) :
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
 
 def dfs(x) :
     visited[x] = True
     cnt = 0
-    
+
     for nx in graph[x] :
         if not visited[nx] :
             cnt += 1
-            dfs(nx)
+            cnt += dfs(nx)
     return cnt
-
-print(dfs(1))
+ans = dfs(1)
+print(ans)
