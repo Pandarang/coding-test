1143fd607cccc7e78db83021fc1374e9515b74cf
조민형 <whalsguddlek@gmail.com>
2026-02-16 13:05:22 +0900

    게임 맵 최단거리 수정

--- programmers/게임 맵 최단거리/solution.py
+++ programmers/게임 맵 최단거리/solution.py
@@ -1,23 +1,23 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps)
-    m = len(maps[0])
-    
+    n = len(maps)      # За
+    m = len(maps[0])   # ї­
+
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
-    
+
     q = deque([(0, 0)])
 
-    while q :
+    while q:
         x, y = q.popleft()
 
-        for i in range(4) :
+        for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 :
-                maps[nx][ny] = maps[x][y] + 1
+            if 0 <= nx < m and 0 <= ny < n and maps[ny][nx] == 1:
+                maps[ny][nx] = maps[y][x] + 1
                 q.append((nx, ny))
 
-    return -1 if maps[n-1][m-1] == 1 else maps[n-1][m-1]
+    return maps[n-1][m-1] if maps[n-1][m-1] != 1 else -1
