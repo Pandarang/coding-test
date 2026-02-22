dfe3a17559b77311552d4798ee530ff1b906fbf5
조민형 <whalsguddlek@gmail.com>
2026-02-22 17:59:59 +0900

    괄호의 값

--- baeckjoon/통나무 건너뛰기/solution.py
+++ baeckjoon/통나무 건너뛰기/solution.py
@@ -1,26 +1,22 @@
 from collections import deque
 
-n = int(input())
-arr = []
+T = int(input())
 
-
-for _ in range(n) :
-    ans = deque()
-    minN = 0
-    int(input())
+for _ in range(T) :
+    n = int(input())
     arr = list(map(int, input().split()))
-    
     arr.sort()
-        
-    for x in range(len(arr)) :
-        if x % 2 == 0 :
-            ans.append(arr[x])
-        else :
-            ans.appendleft(arr[x])
+    q = deque()
+    q.append(arr.pop())
+    
+    while arr :
+        if arr :
+            q.append(arr.pop())
+        if arr :
+            q.appendleft(arr.pop())
+            
+    max_num = 0
+    for i in range(1, len(q)) :
+        max_num = max(q[i] - q[i-1], max_num)
 
-    for x in range(len(ans) - 1) :
-        temp = abs(ans[x] - ans[x+1])
-        if temp > minN :
-            minN = temp
-        
-    print(minN)
+    print(max_num)
