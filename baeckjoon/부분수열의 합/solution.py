21b755950183207f0316334bab954648aff2db04
조민형 <whalsguddlek@gmail.com>
2026-03-10 16:55:07 +0900

    2xn 타일링

--- baeckjoon/부분수열의 합/solution.py
+++ baeckjoon/부분수열의 합/solution.py
@@ -1,22 +1,5 @@
-n, s = map(int, input().split())
+from itertools import combinations
+
+n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-answer = 0
-
-def dfs(idx, total) :
-    global answer
-    if idx == n :
-        if total == s :
-            answer += 1
-        return
-    
-    dfs(idx + 1, total + arr[idx])
-    
-    dfs(idx + 1, total)
-
-dfs(0, 0)
-
-if s == 0 :
-    answer -= 1
-    
-print(answer)
