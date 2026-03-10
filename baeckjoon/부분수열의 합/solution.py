c4205942c07ffaa4a25d83441648f740a46a93a0
조민형 <whalsguddlek@gmail.com>
2026-03-10 17:09:32 +0900

    부분수열의 합

--- baeckjoon/부분수열의 합/solution.py
+++ baeckjoon/부분수열의 합/solution.py
@@ -1,5 +1,21 @@
-from itertools import combinations
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
+answer = 0
+
+def dfs(idx, total) :
+    global answer
+    if idx == n :
+        if total == m :
+            answer += 1
+        return
+            
+    dfs(idx + 1, total + arr[idx])
+    dfs(idx + 1, total)
+
+dfs(0, 0)
+
+if m == 0 :
+    answer -= 1
+    
+print(answer)
